fdff82355d07b960148d7933a58d24ed5e5d8821
Gustavo Rosano <gurosano@gmail.com>
2021-09-02 18:21:50 -0300

    Testando Componentes

--- Exercicios/src/App.js
+++ Exercicios/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
 
+import Comp, { Comp1, Comp2 } from './components/Multi'
 import Primeiro from './components/Primeiro'
 
 
 export default () => (
     <SafeAreaView>
+
+        <Comp />
+        <Comp1 />
+        <Comp2 />
         <Primeiro />
+        
     </SafeAreaView>
 )
