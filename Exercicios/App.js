376ebf0a5bedab001072cb32165057ce3c1a0f04
Gustavo Rosano <gurosano@gmail.com>
2021-09-08 21:23:06 -0300

    Testando Multiplos Componentes

--- Exercicios/App.js
+++ Exercicios/App.js
@@ -2,10 +2,16 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text,  SafeAreaView } from 'react-native';
 
-export default function App() {
+import Comp, { Comp1, Comp2 } from './src/components/Multi'
+import Primeiro from './src/components/Primeiro'
+
+export default () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Primeiro Componente</Text>
+      <Comp />
+      <Comp1 />
+      <Comp2 />
+      <Primeiro />
       <StatusBar style="auto" /> 
     </SafeAreaView>
   );
