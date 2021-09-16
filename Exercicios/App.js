78bb1c0dec45a5408d082c6e9f58de082ddb814f
Gustavo Rosano <gurosano@gmail.com>
2021-09-16 18:27:33 -0300

    Fragmento

--- Exercicios/App.js
+++ Exercicios/App.js
@@ -1,27 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet,  SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native';
 
+import Titulo from './src/components/Titulo'
+/*
 import Aleatorio from './src/components/Aleatorio'
-/* import MinMax from './src/components/MinMax'
+import MinMax from './src/components/MinMax'
 import Comp, { Comp1, Comp2 } from './src/components/Multi'
 import Primeiro from './src/components/Primeiro'
  */
 export default () => {
   return (
     <SafeAreaView style={styles.App}>
+      <Titulo principal="Cadastro de Produto" />
+      <Titulo secundario="Tela de Cadastro do Produto" />
+      {/* 
       <Aleatorio min={1} max={60} />
       <Aleatorio min={1} max={60} />
       <Aleatorio min={1} max={60} />
       <Aleatorio min={1} max={60} />
       <Aleatorio min={1} max={60} />
       <Aleatorio min={1} max={60} />
-      {/* <MinMax min={3} max={20} /> 
+      <MinMax min={3} max={20} /> 
       <Comp />
       <Comp1 />
       <Comp2 />
       <Primeiro /> */}
-      <StatusBar style="auto" /> 
+      <StatusBar style="auto" />
     </SafeAreaView>
   );
 }
