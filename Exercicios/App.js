9a059d92dfa5d4a41f6d2395e44dacca6b8805ef
Gustavo Rosano <gurosano@gmail.com>
2021-09-17 18:03:46 -0300

    Diferenciando IOS e Android

--- Exercicios/App.js
+++ Exercicios/App.js
@@ -2,8 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 
-import ContadorV2 from './src/components/contador/ContadorV2'
+import Diferenciar from './src/components/Diferenciar';
 /*
+import ContadorV2 from './src/components/contador/ContadorV2'
 import PaiI from './src/components/indireta/Pai'
 import Pai from './src/components/direta/Pai'
 import Contador from './src/components/Contador'
@@ -17,8 +18,9 @@ import Primeiro from './src/components/Primeiro'
 export default () => {
   return (
     <SafeAreaView style={styles.App}>
-      <ContadorV2 />
+      <Diferenciar />
       {/* 
+      <ContadorV2 />
       <PaiI />
       <Pai />
       <Contador inicial={100} passo={13} />
